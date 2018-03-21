ab3c7f517f4e7399ef34cd682833df064a3609fd
root <root@osmolyakova.croc.ru>
2018-03-21 18:41:05 +0300

    aff tldp over rsvp

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@
 
 
 
-ansible-playbook plays/make_cfg.yml --e target=all
+#ansible-playbook plays/make_cfg.yml --e target=iosxr
 
-ansible-playbook -i hosts plays/deploy_cfg.yml --e commit=1 --e target=all -f 10
+#ansible-playbook -i hosts plays/deploy_cfg.yml --e commit=1 --e target=iosxr -f 10
 
+ansible-playbook -i hosts plays/mk_and_dp.yml --e commit=1 --e target=all -f 10
