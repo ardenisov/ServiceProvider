18f3c735d263d2b997ca4be2cad76d30ae4892c1
root <root@azakharov-n.croc.ru>
2018-03-29 16:38:34 +0300

    add LFA

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@
 
 #ansible-playbook -i hosts plays/deploy_cfg.yml --e commit=1 --e target=iosxr -f 10
 
-ansible-playbook -i hosts plays/mk_and_dp.yml --e commit=1 --e target=all -f 10
+ansible-playbook -i hosts plays/mk_and_dp.yml --e commit=1 --e target=junos -f 10
