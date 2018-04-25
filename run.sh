530650a4a05b455a1ab605c59a20dcf442618e64
ardenisov <denart88@yandex.ru>
2018-04-25 15:15:23 +0300

    add provider tunnel p2mp  ldp

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@ export ANSIBLE_CONFIG=/etc/ansible/SP/ansible.cfg
 
 
 
-ansible-playbook -i ./hosts plays/make_cfg.yml --e target=iosxr
+ansible-playbook -i ./hosts plays/make_cfg.yml --e target=junos
 
-ansible-playbook -i hosts plays/deploy_cfg.yml --e commit=1 --e target=iosxr -f 10
+ansible-playbook -i hosts plays/deploy_cfg.yml --e commit=1 --e target=junos -f 10
 
 #ansible-playbook -i ./hosts plays/mk_and_dp.yml --e commit=1 --e target=iosxr -f 10
